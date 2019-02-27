81f785a875f600d6f3ebf7be127a3ee79eade18b
blackbat13 <blackbat13@gmail.com>
2019-02-27 10:50:36 +0100

    Dodano rozwiązania ćwiczeń w Pythonie

---
+++ Wprowadzenie do zmiennych/II. Wprowadzenie do zmiennych/Python/cw1.py
@@ -0,0 +1,15 @@
+a = "jabłko"
+b = "gruszka"
+
+print("Po inicjalizacji:")
+print("a =", a)
+print("b =", b)
+
+c = a
+a = b
+b = c
+
+print()
+print("Po zamianie:")
+print("a =", a)
+print("b =", b)
---
+++ Wprowadzenie do zmiennych/II. Wprowadzenie do zmiennych/Python/cw6.py
@@ -0,0 +1,6 @@
+n = int(input("Podaj n:"))
+suma = 0
+for i in range(1, n + 1):
+    suma += i
+
+print("suma =", suma)
